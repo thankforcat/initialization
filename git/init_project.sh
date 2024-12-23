ebce447b55617381de868ff54f0ea61e0d907f8e
thankforcat <ffslmo.wmy@gmail.com>
2024-12-23 10:26:37 -0800

    修改.gitignore 0.1.2

--- git/init_project.sh
+++ git/init_project.sh
@@ -22,16 +22,51 @@ git checkout -b dev
 echo "# $project_name" > README.md
 
 # 创建 .gitignore 文件
-echo "node_modules/
-.DS_Store
-*.log
-src/images/
-.vscode
-.gitignore
-src/config/
-images
+echo "*.log
 *.db
-.env" > .gitignore
+
+# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.*
+.yarn/*
+!.yarn/patches
+!.yarn/plugins
+!.yarn/releases
+!.yarn/versions
+
+# testing
+/coverage
+
+# next.js
+/.next/
+/out/
+
+# production
+/build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+.pnpm-debug.log*
+
+# env files (can opt-in for committing if needed)
+.env*
+
+# vercel
+.vercel
+
+# typescript
+*.tsbuildinfo
+next-env.d.ts
+" > .gitignore
 
 # 添加文件到 Git 并提交
 git add .
