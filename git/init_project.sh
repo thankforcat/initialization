8fd772fe95bd723b87d4584c54fffc3c018c6fcb
thankforcat <ffslmo.wmy@gmail.com>
2024-12-23 10:00:30 -0800

    添加 cd 进入文件0.1.1

--- git/init_project.sh
+++ git/init_project.sh
@@ -2,19 +2,18 @@
 
 # bash <(curl -Ls https://raw.githubusercontent.com/thankforcat/initialization/refs/heads/main/git/init_project.sh)
 # https://github.com/thankforcat/initialization
+
 # 提示用户输入项目名称
 read -p "请输入你的项目名称: " project_name
 
-# 创建项目目录
-mkdir "$project_name" 
+# 创建项目目录并进入
+mkdir "$project_name" && cd "$project_name"
 
 # 初始化 Git 仓库
 git init
 
 # 创建 dev 分支
 git checkout -b dev
-
-
 
 # 创建 README.md 文件
 echo "# $project_name" > README.md
@@ -46,15 +45,4 @@ if [ "$repo_type" -eq 2 ]; then
     gh repo create "$project_name" --public --source=. --remote=origin --push
     echo "公开仓库已创建并推送！"
 else
-    gh repo create "$project_name" --private --source=. --remote=origin --push
-    echo "私人仓库已创建并推送！"
-fi
-
-# 提示完成
-echo "项目 $project_name 已创建，包含 README.md 和 .gitignore 文件，并已推送到 GitHub！"
-
-cd "$project_name"
-
-
-# 打开 VS Code
-# code .
+    gh repo create "$project_n
